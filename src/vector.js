4e9107f36d83e592040b956afdeb65f874b3c80d
Marty Kane <marty.kane@gmail.com>
2020-12-29 21:19:13 -0600

    reorder code

--- src/vector.js
+++ src/vector.js
@@ -5,6 +5,10 @@ export function add(v1 = {x: 0, y: 0}, v2 = {x: 0, y: 0}) {
     }
 }
 
+export function dot(v1 = {x: 0, y: 0}, v2 = {x: 0, y: 0}) {
+    return (v1.x || 0) * (v2.x || 0) + (v1.y || 0) * (v2.y || 0)
+}
+
 export function reverse({x = -0, y = -0} = {}) {
     return {
         x: x * -1 || 0,
@@ -17,10 +21,6 @@ export function subtract(v1 = {x: 0, y: 0}, v2 = {x: 0, y: 0}) {
         x: _(v1.x) - _(v2.x),
         y: _(v1.y) - _(v2.y),
     }
-}
-
-export function dot(v1 = {x: 0, y: 0}, v2 = {x: 0, y: 0}) {
-    return (v1.x || 0) * (v2.x || 0) + (v1.y || 0) * (v2.y || 0)
 }
 
 function _(n) {
--- test/vector.test.js
+++ test/vector.test.js
@@ -54,6 +54,66 @@ describe('Adding two Vectors', () => {
     })
 })
 
+describe('Dot Product of two Vectors', () => {
+    it(`returns 0 for bad inputs`, () => {
+        expect(dot()).to.equal(0)
+        expect(dot(true)).to.equal(0)
+        expect(dot(1)).to.equal(0)
+        expect(dot('')).to.equal(0)
+        expect(dot([])).to.equal(0)
+        expect(dot({x: 1, y: 1})).to.equal(0)
+    })
+
+    it(`does not modify the arguments`, () => {
+        const input1 = {x: 1, y: 1}
+        const input2 = {x: 2, y: 2}
+        deepFreeze(input1);
+        deepFreeze(input2);
+
+        expect(_ => dot(input1, input2)).to.not.throw();
+    })
+
+    it(`returns a number value`, () => {
+        expect(dot({x: 0, y: 1}, {x: 1, y: 1})).to.equal(1)
+        expect(dot({x: .5, y: .5}, {x: 1, y: 1})).to.equal(1)
+        expect(dot({x: 1, y: 1}, {x: 1, y: 1})).to.equal(2)
+        expect(dot({x: 1, y: 2}, {x: 3, y: 4})).to.equal(11)
+        expect(dot({x: -5, y: 5}, {x: 5, y: -5})).to.equal(-50)
+    })
+})
+
+describe('Reversing a Vector', () => {
+    it(`returns {x: 0, y: 0} for bad inputs`, () => {
+        expect(reverse()).to.deep.equal(zeroVector)
+        expect(reverse(true)).to.deep.equal(zeroVector)
+        expect(reverse(1)).to.deep.equal(zeroVector)
+        expect(reverse('')).to.deep.equal(zeroVector)
+        expect(reverse([])).to.deep.equal(zeroVector)
+    })
+
+    it(`fills missing or bad values with zeros`, () => {
+        expect(reverse({})).to.deep.equal(zeroVector)
+        expect(reverse({x: 'bad'})).to.deep.equal(zeroVector)
+        expect(reverse({x: 0})).to.deep.equal(zeroVector)
+        expect(reverse({x: 0, y: 'bad'})).to.deep.equal(zeroVector)
+    })
+
+    it(`does not modify the old vector and returns a new one`, () => {
+        const input = {x: 1, y: 1}
+        deepFreeze(input);
+
+        expect(_ => reverse(input)).to.not.throw();
+        expect(reverse(input)).to.not.equal(input);
+    })
+
+    it(`negates positive values and negative values`, () => {
+        expect(reverse({x: 1, y: 1})).to.deep.equal({x: -1, y: -1})
+        expect(reverse({x: -1, y: -1})).to.deep.equal({x: 1, y: 1})
+        expect(reverse({x: 1, y: -1})).to.deep.equal({x: -1, y: 1})
+        expect(reverse({x: -1, y: 1})).to.deep.equal({x: 1, y: -1})
+    })
+})
+
 describe('Subtracting two Vectors', () => {
     it(`returns {x: 0, y: 0} for bad inputs`, () => {
         expect(subtract()).to.deep.equal(zeroVector)
@@ -93,65 +153,5 @@ describe('Subtracting two Vectors', () => {
         expect(subtract({x: 1, y: 1}, {x: 2, y: 3})).to.deep.equal({x: -1, y: -2})
         expect(subtract({x: -5, y: -5}, {x: 10, y: 15})).to.deep.equal({x: -15, y: -20})
         expect(subtract({x: 10, y: 10}, {x: 9, y: 9})).to.deep.equal({x: 1, y: 1})
-    })
-})
-
-describe('Reversing a Vector', () => {
-    it(`returns {x: 0, y: 0} for bad inputs`, () => {
-        expect(reverse()).to.deep.equal(zeroVector)
-        expect(reverse(true)).to.deep.equal(zeroVector)
-        expect(reverse(1)).to.deep.equal(zeroVector)
-        expect(reverse('')).to.deep.equal(zeroVector)
-        expect(reverse([])).to.deep.equal(zeroVector)
-    })
-
-    it(`fills missing or bad values with zeros`, () => {
-        expect(reverse({})).to.deep.equal(zeroVector)
-        expect(reverse({x: 'bad'})).to.deep.equal(zeroVector)
-        expect(reverse({x: 0})).to.deep.equal(zeroVector)
-        expect(reverse({x: 0, y: 'bad'})).to.deep.equal(zeroVector)
-    })
-
-    it(`does not modify the old vector and returns a new one`, () => {
-        const input = {x: 1, y: 1}
-        deepFreeze(input);
-
-        expect(_ => reverse(input)).to.not.throw();
-        expect(reverse(input)).to.not.equal(input);
-    })
-
-    it(`negates positive values and negative values`, () => {
-        expect(reverse({x: 1, y: 1})).to.deep.equal({x: -1, y: -1})
-        expect(reverse({x: -1, y: -1})).to.deep.equal({x: 1, y: 1})
-        expect(reverse({x: 1, y: -1})).to.deep.equal({x: -1, y: 1})
-        expect(reverse({x: -1, y: 1})).to.deep.equal({x: 1, y: -1})
-    })
-})
-
-describe('Dot Product of two Vectors', () => {
-    it(`returns 0 for bad inputs`, () => {
-        expect(dot()).to.equal(0)
-        expect(dot(true)).to.equal(0)
-        expect(dot(1)).to.equal(0)
-        expect(dot('')).to.equal(0)
-        expect(dot([])).to.equal(0)
-        expect(dot({x: 1, y: 1})).to.equal(0)
-    })
-
-    it(`does not modify the arguments`, () => {
-        const input1 = {x: 1, y: 1}
-        const input2 = {x: 2, y: 2}
-        deepFreeze(input1);
-        deepFreeze(input2);
-
-        expect(_ => dot(input1, input2)).to.not.throw();
-    })
-
-    it(`returns a number value`, () => {
-        expect(dot({x: 0, y: 1}, {x: 1, y: 1})).to.equal(1)
-        expect(dot({x: .5, y: .5}, {x: 1, y: 1})).to.equal(1)
-        expect(dot({x: 1, y: 1}, {x: 1, y: 1})).to.equal(2)
-        expect(dot({x: 1, y: 2}, {x: 3, y: 4})).to.equal(11)
-        expect(dot({x: -5, y: 5}, {x: 5, y: -5})).to.equal(-50)
     })
 })
