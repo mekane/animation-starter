14379ee7a6f48d362ce38c7b75d1b0e8391cfdef
Marty Kane <marty.kane@bisonok.com>
2021-11-25 09:56:13 -0600

    Add tests for controlling acceleration

--- test/SpaceGame.test.js
+++ test/SpaceGame.test.js
@@ -4,17 +4,19 @@ const expect = chai.expect;
 import { SpaceGamePlugin } from "../spaceGame/SpaceGame.js";
 
 describe('SpaceGame Plugin', () => {
-    let plugin;
+    let plugin
+    let state
+    let blackHole
 
     beforeEach(() => {
         plugin = new SpaceGamePlugin();
+        state = plugin.getInitialState()
+        blackHole = findBlackHole(state.entities)
     })
 
     it('sets up initial state', () => {
-        const initialState = plugin.getInitialState()
-        expect(initialState).to.be.an('object')
-        expect(initialState.entities.length).to.be.greaterThan(5)
-        const blackHole = findBlackHole(initialState.entities)
+        expect(state).to.be.an('object')
+        expect(state.entities.length).to.be.greaterThan(5)
         expect(blackHole).to.be.an('object')
     })
 
@@ -25,13 +27,51 @@ describe('SpaceGame Plugin', () => {
         expect(blackHole.y).to.equal(100)
     })
 
-    it('accelerates the black hole when arrow keys are pressed', () => {
-        const plugin = new SpaceGamePlugin()
-        const state = plugin.getInitialState();
+    it('accelerates the black hole up when up arrow key is pressed', () => {
         const controls = { up: true }
         plugin.preUpdate(state, controls)
 
+        const velocity = blackHole.velocity
+        expect(velocity.x).to.equal(0)
+        expect(velocity.y).to.be.greaterThan(0)
     })
+
+    it('accelerates the black hole down when down arrow key is pressed', () => {
+        const controls = { down: true }
+        plugin.preUpdate(state, controls)
+
+        const velocity = blackHole.velocity
+        expect(velocity.x).to.equal(0)
+        expect(velocity.y).to.be.lessThan(0)
+    })
+
+    it('accelerates the black hole left when left arrow key is pressed', () => {
+        const controls = { left: true }
+        plugin.preUpdate(state, controls)
+
+        const velocity = blackHole.velocity
+        expect(velocity.x).to.be.lessThan(0)
+        expect(velocity.y).to.equal(0)
+    })
+
+    it('accelerates the black hole right when right arrow key is pressed', () => {
+        const controls = { right: true }
+        plugin.preUpdate(state, controls)
+
+        const velocity = blackHole.velocity
+        expect(velocity.x).to.be.greaterThan(0)
+        expect(velocity.y).to.equal(0)
+    })
+
+    it('combines accelerations with multiple keys', () => {
+        const controls = { down: true, left: true }
+        plugin.preUpdate(state, controls)
+
+        const velocity = blackHole.velocity
+        expect(velocity.x).to.be.lessThan(0)
+        expect(velocity.y).to.be.lessThan(0)
+    })
+
 })
 
 function findBlackHole(entityList) {
