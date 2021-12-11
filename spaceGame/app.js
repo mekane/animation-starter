aa0cacba42a8ff1a72bdd79735c3383c7dd8ca23
Marty Kane <marty.kane@gmail.com>
2021-12-11 10:07:05 -0600

    split the JS out into a separate file, inline CSS

---
+++ spaceGame/app.js
@@ -0,0 +1,11 @@
+import {Game} from '../src/Game.js';
+import {HtmlView} from "../src/View.js";
+import {BrowserTimer} from "../src/Timer.js";
+import {BrowserControls} from "../src/Controls.js";
+import {SpaceGamePlugin} from "./SpaceGame.js";
+
+const controls = new BrowserControls()
+const view = HtmlView(window, document.getElementById('main'))
+const timer = new BrowserTimer();
+const game = Game(controls, view, timer, new SpaceGamePlugin(), {walls: true});
+
