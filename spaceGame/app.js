61d7587f79bc3f5cec228d18fef4d5dcb92f3338
Marty Kane <marty.kane@gmail.com>
2021-12-15 11:48:25 -0600

    Move Help to its own script and import it into the space game app

--- spaceGame/app.js
+++ spaceGame/app.js
@@ -3,6 +3,7 @@ import {HtmlView} from "../src/View.js";
 import {BrowserTimer} from "../src/Timer.js";
 import {BrowserControls} from "../src/Controls.js";
 import {SpaceGamePlugin} from "./SpaceGame.js";
+import "../src/Help.js";
 
 const controls = new BrowserControls()
 const view = HtmlView(window, document.getElementById('main'))
